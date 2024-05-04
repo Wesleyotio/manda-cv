c3e4b000b74c1013c972c7f84e51fa2542d5a3aa
José Wesley Gonçalves <josewg@algartelecom.com.br>
2024-05-04 20:48:57 -0300

    created model candidates

---
+++ app/Models/Candidate.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'job',
+        'education_level',
+        'obs',
+        'doc_name_cv',
+        'ip_candidate'
+    ];
+}
