0ca0c8bea9d93ef0e6f4c5b0d2041e36f678bc1c
José Wesley Gonçalves <josewg@algartelecom.com.br>
2024-05-04 19:30:58 -0300

    created table candidates

---
+++ database/migrations/2024_05_04_211845_create_candidates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('job');
+            $table->integer('education_level');
+            $table->string('doc_name_cv');
+            $table->string('ip_candidate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
