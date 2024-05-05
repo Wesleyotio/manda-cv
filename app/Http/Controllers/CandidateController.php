d7a67a8dc2418dc9a6002f4456dcbe2d9389c5d7
José Wesley Gonçalves <josewg@algartelecom.com.br>
2024-05-05 20:29:41 -0300

    update test and controller for accepted file types doc,docx and pdf

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Enums\EducationLevel;
+use App\Models\Candidate;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CandidateController extends Controller
 {
@@ -28,17 +30,41 @@ class CandidateController extends Controller
             'job' => 'required|string|max:255',
             'education_level' => 'required|integer',
             'obs' => 'nullable|string|max:255' , 
-            'doc_name_cv' => 'required|string',
             'ip_candidate' => 'required|string|max:15'
         ]);
         
         try {
             
+            if ($request->hasFile('doc_name_cv')) {
+
+                $doc_cv = $request->file('doc_name_cv');
+                if ($doc_cv->isValid() && $this->isValidMimeType($doc_cv->getMimeType())) {
+
+                    $path = Storage::putFile('docs', $doc_cv);
+                    $candidate = Candidate::create([
+                        'name' => $request->name,
+                        'email' => $request->email,
+                        'phone' => $request->phone,
+                        'job' => $request->job,
+                        'education_level' => $request->education_level,
+                        'obs' => $request->obs, 
+                        'doc_name_cv' => $path,
+                        'ip_candidate' => $request->ip_candidate
+                    ]);
+        
+                    $response = [
+                        'success' => true,
+                        'message' => 'Currículo enviado com sucesso',
+                    ];
+                    return response()->json($response, 200);
+                }
+            }
+
             $response = [
-                'success' => true,
-                'message' => 'Currículo enviado com sucesso',
+                'success' => false,
+                'message' => "Falha ao processar o arquivo!",
             ];
-            return response()->json($response, 200);
+            return response()->json($response, 400);
         } catch (QueryException $qe) {
             $success = false;
             $message = $qe;
@@ -48,6 +74,24 @@ class CandidateController extends Controller
                 'message' => $message,
             ];
             return response()->json($response, 500);
+        }
+    }
+
+    private function isValidMimeType($mimeType): bool
+    {
+        switch ($mimeType) {
+            case 'application/pdf':
+                return true;
+                break;
+            case 'application/msword':
+                return true;
+                break;
+            case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                return true;
+                break;
+            default:
+                return false;
+                break;
         }
     }
 }
--- tests/Feature/CandidateTest.php
+++ tests/Feature/CandidateTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Enums\EducationLevel;
 use Faker\Factory;
 use Illuminate\Http\Response;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class CandidateTest extends TestCase
@@ -35,6 +37,10 @@ class CandidateTest extends TestCase
         $faker->addProvider(new \Faker\Provider\pt_BR\PhoneNumber($faker));
         $faker->addProvider(new \Faker\Provider\Internet($faker));
 
+        Storage::fake('storage');
+
+        $file = UploadedFile::fake()->create('cv_test.pdf');
+
         $response = $this->post('api/send-cv',
         [
             "Accept"=>"application/json",
@@ -43,10 +49,11 @@ class CandidateTest extends TestCase
             'phone' => $faker->phoneNumberCleared,
             'job' => $faker->word,
             'education_level' => rand(1,5),
+            'doc_name_cv' => $file,
             'obs' => $faker->sentence($nbWords = 6, $variableNbWords = true) , 
-            'doc_name_cv' => $faker->word,
             'ip_candidate' => $faker->ipv4
         ]);
+
 
         $response->assertStatus(Response::HTTP_OK)
             ->assertJson([
