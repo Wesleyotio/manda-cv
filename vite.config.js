f7f211f81fc7570acde849a7e307f9af88d7ea73
José Wesley Gonçalves <josewg@algartelecom.com.br>
2024-05-12 18:11:09 -0300

    configuration for support of vue on the vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import vue from '@vitejs/plugin-vue';
+ 
 export default defineConfig({
+    server: {
+        port:   9000,
+        cors:   false,
+        origin: 'http://localhost'
+    },
+    define: {
+        // enable hydration mismatch details in production build
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
 });
