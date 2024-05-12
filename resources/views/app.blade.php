adec497a38895f0fa09b9f65380e0d42ca824011
José Wesley Gonçalves <josewg@algartelecom.com.br>
2024-05-12 18:07:27 -0300

    changed for support of vueJS

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{-- <meta name="csrf-token" content="{{ csrf_token() }}"/> --}}
+        <title>Formulario de candidato </title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        {{-- @vite('resources/css/app.css') --}}
+    </head>
+    <body>
+        <div id="app"></div>
+        @vite('resources/js/app.js')
+    </body>
+</html>
